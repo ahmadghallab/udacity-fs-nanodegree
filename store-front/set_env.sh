584a91401a4d1d1bfda1dc5c88513712e6dc1a87
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2022-11-08 20:03:00 +0200

    deploy script update

---
+++ store-front/set_env.sh
@@ -0,0 +1,12 @@
+export POSTGRES_HOST=database-1.crz81ettzcoa.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=shopping
+export POSTGRES_DB_TEST=shopping_test
+export POSTGRES_USER=shopping_user
+export POSTGRES_PASSWORD=password123
+export JWT_KEY=12345abcd
+export ENV=dev
+export PORT=3002
+export AWS_BUCKET=arn:aws:s3:::fs-frontend
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export URL=http://localhost:3000
